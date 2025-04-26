9e3c143738fe8c26d8162dc6d9bb4332e3734153
Yerson Garza <vladimir.ygl@gmail.com>
2025-04-26 06:43:15 -0300

    Exercise 1.1 done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,22 @@
+const Header = ({ course }) => { 
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+  return (
+    <div>
+        <p>{part1} {exercises1}</p>
+        <p>{part2} {exercises2}</p>
+        <p>{part3} {exercises3}</p>
+    </div>        
+  )
+}
+
+const Total = ({exercises1, exercises2, exercises3}) => {
+  return <p>Number of exercises {exercises1+exercises2+exercises3}</p> 
+}
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -8,26 +27,22 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+
   return (
     <div>
       <Header course={course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p> Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content 
+        part1={part1} exercises1={exercises1} 
+        part2={part2} exercises2={exercises2} 
+        part3={part3} exercises3={exercises3}
+      />
+      <Total 
+        exercises1={exercises1} 
+        exercises2={exercises2} 
+        exercises3={exercises3}
+      />
     </div>
   )
-}
-
-const Header =  ({course}) => { 
-  return <h1>{course}</h1>
-  
 }
 
 export default App
